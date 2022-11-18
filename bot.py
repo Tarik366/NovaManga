785c9c4586a75939e0d043ac0493cfcffac019ae
Tarık Yıldız <67359075+Tarik366@users.noreply.github.com>
2022-11-18 18:02:26 +0300

    Final(gerçekten)

--- bot.py
+++ bot.py
@@ -16,46 +16,67 @@ load_dotenv()
 
 @Bot.event
 async def on_ready():
-    NF = feedparser.parse("https://nova-manga.com/feed/")
+    NF = feedparser.parse("https://athenafansub.com/feed/")
     fentry = NF.entries[0]
     msg1.start()
 
-@tasks.loop(minutes=1)
+@tasks.loop(minutes=3)
 async def msg1():
+    
+    
     NF = feedparser.parse("https://nova-manga.com/feed/")
     entry = NF.entries[0]
+    
+    
     le = open("lastEntry.txt", "r", encoding="utf-8")
     ar = le.read()
+    
+    
+    class n:
+        title = entry.title
+        link = entry.link
+        cat = entry.category
+        catt = cat.replace(" ", "-")
+        catt = catt.replace("'", "").lower()
+        
+        
+    h = "https://nova-manga.com/manga/" + n.catt + "/"
     sentry = entry.title
+    
+    
     if sentry not in str(ar):
-        class n:
-            title = entry.title
-            link = entry.link
-            cat = entry.category
-            catt = cat.replace(" ", "-")
-            catt = catt.replace("'", "").lower()
-
-        h = "https://nova-manga.com.com/manga/" + n.catt + "/"
         print(h)
 
         headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
         sou = requests.get(h, headers=headers)
         soup = BeautifulSoup(sou.content, 'html.parser')
-        im = ["a"]
-        for s in soup.find_all("div", class_="thumb"):
-            for item in s.find_all('img', class_="wp-post-image"):
-                i = item['src']
-                im = im + i.split("\n")
-                print(im[1])
-        n.img = im[1]
-        emed = Embed(title=f"{entry.title} yayında keyifli okumalar!", description=f"okumak için {entry.link}", url=entry.link)
-        emed = emed.set_image(url = n.img)
-        channel = Bot.get_channel(a)
-        print(str(ar))
-        with open("lastEntry.txt", "w", encoding="utf-8") as wle:
-            wle.write(n.title)
-        await channel.send(embed=emed)
-    print("a")
+        
+        
+        hgf = "https://athenafansub.com/wp-content/themes/mangastream/assets/images/noimg165px.png"
+        
+        def ws():
+            for s in soup.find_all("div", class_="thumb"):
+                
+                
+                for item in s.find_all('img', class_="wp-post-image"):
+                    i = item['src']
+                    im = i.split("\n")
+                    hgf = im[0]
+                    print(hgf)
+                    return hgf
 
+        nimg = ws()
+        emed = Embed(title=f"{entry.title} yayında keyifli okumalar!", description=f"okumak için {entry.link}", url=entry.link)
+        emed = emed.set_image(url = nimg)
+
+        channel = Bot.get_channel(a)
+
+        print(str(ar))
+
+        with open("lastEntry.txt", "w", encoding="utf-8") as wle:
+            wle.write(sentry)
+
+        await channel.send(embed=emed)
+    print(h)
 
 Bot.run(os.getenv('token')) 

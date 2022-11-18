ac71641a281d287b2bfddafe25dc5eadcdbe4380
Tarık Yıldız <67359075+Tarik366@users.noreply.github.com>
2022-11-18 06:14:12 +0300

    a

--- bot.py
+++ bot.py
@@ -5,8 +5,9 @@ from discord.ext import commands, tasks
 import os.path
 from dotenv.main import load_dotenv
 import datetime as dt
+import requests
 import feedparser
-from RSS import *
+from bs4 import BeautifulSoup
 
 a = 1034908325336334389 
 intents = Intents.all()
@@ -15,31 +16,46 @@ load_dotenv()
 
 @Bot.event
 async def on_ready():
-    NF = feedparser.parse("https://nova-manga.com/feed/")
+    NF = feedparser.parse("https://athenafansub.com/feed/")
     fentry = NF.entries[0]
     msg1.start()
 
-@tasks.loop(minutes=10)
+@tasks.loop(minutes=1)
 async def msg1():
-    NF = feedparser.parse("https://nova-manga.com/feed/")
+    NF = feedparser.parse("https://athenafansub.com/feed/")
     entry = NF.entries[0]
-    le = open("lastEntry.txt", "r")
+    le = open("lastEntry.txt", "r", encoding="utf-8")
     ar = le.read()
-    sentry = str(entry)
+    sentry = entry.title
     if sentry not in str(ar):
+        class n:
+            title = entry.title
+            link = entry.link
+            cat = entry.category
+            catt = cat.replace(" ", "-")
+            catt = catt.replace("'", "").lower()
+
+        h = "https://athenafansub.com/manga/" + n.catt + "/"
+        print(h)
+
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        sou = requests.get(h, headers=headers)
+        soup = BeautifulSoup(sou.content, 'html.parser')
+        im = ["a"]
+        for s in soup.find_all("div", class_="thumb"):
+            for item in s.find_all('img', class_="wp-post-image"):
+                i = item['src']
+                im = im + i.split("\n")
+                print(im[1])
+        n.img = im[1]
         emed = Embed(title=f"{entry.title} yayında keyifli okumalar!", description=f"okumak için {entry.link}", url=entry.link)
         emed = emed.set_image(url = n.img)
         channel = Bot.get_channel(a)
-        print(emed)
-        with open("lastEntry.txt", "w") as wle:
-            wle.write(sentry)
+        print(str(ar))
+        with open("lastEntry.txt", "w", encoding="utf-8") as wle:
+            wle.write(n.title)
         await channel.send(embed=emed)
+    print("a")
 
-
-@Bot.command()
-async def o(ctx):
-    emed = Embed(title=f"{n.title} yayında keyifli okumalar!", description=f"okumak için {n.link}", url=n.link)
-    emed = emed.set_image(url = n.img)
-    await ctx.send(embed=emed)
 
 Bot.run(os.getenv('token')) 
